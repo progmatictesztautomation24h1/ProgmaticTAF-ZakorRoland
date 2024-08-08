7298c1c6e69ca0cbaad0c45a9f8fae3d3f4c44a2
zsenyuk <tamas.zsenyuk@getbridge.com>
2024-08-08 18:18:54 +0200

    all basic examples added

--- src/main/java/api/PetStoreApiTest.java
+++ src/main/java/api/PetStoreApiTest.java
@@ -1,8 +1,6 @@
 package api;
 
-
 import static io.restassured.RestAssured.*;
-
 import org.testng.annotations.Test;
 
 public class PetStoreApiTest {
@@ -34,8 +32,6 @@ public class PetStoreApiTest {
                 .assertThat().statusCode(200);
     }
 
-
-
     @Test
     public void postNewPet() {
         String postBody = "{\n" +
@@ -63,6 +59,62 @@ public class PetStoreApiTest {
         .when()
                 .post("https://petstore.swagger.io/v2/pet")
         .then()
+                .assertThat().statusCode(200);
+    }
+
+    @Test
+    public void changeAlreadyExistingPetWithPut() {
+        String postBody = "{\n" +
+                "  \"id\": 923749237121,\n" +
+                "  \"category\": {\n" +
+                "    \"id\": 0,\n" +
+                "    \"name\": \"string\"\n" +
+                "  },\n" +
+                "  \"name\": \"Asztor\",\n" +
+                "  \"photoUrls\": [\n" +
+                "    \"https://www.purina.co.uk/sites/default/files/2020-12/Dog_1098119012_Teaser.jpg\"\n" +
+                "  ],\n" +
+                "  \"tags\": [\n" +
+                "    {\n" +
+                "      \"id\": 0,\n" +
+                "      \"name\": \"string\"\n" +
+                "    }\n" +
+                "  ],\n" +
+                "  \"status\": \"sold\"\n" +
+                "}";
+
+        given()
+                .body(postBody)
+                .header("Content-Type", "application/json")
+        .when()
+                .put("https://petstore.swagger.io/v2/pet")
+        .then()
+                .assertThat().statusCode(200);
+    }
+
+    @Test
+    public void tryToChangeAlreadyExistingPetWithPatch() {
+        String postBody = "{\n" +
+                "  \"id\": 923749237121,\n" +
+                "  \"name\": \"Asztor new\"\n" +
+                "}";
+
+        given()
+                .body(postBody)
+                .header("Content-Type", "application/json")
+        .when()
+                .patch("https://petstore.swagger.io/v2/pet")
+       .then()
+                .assertThat().statusCode(405);
+    }
+
+
+    @Test
+    public void deletePetById() {
+        given()
+                .when()
+                .delete("https://petstore.swagger.io/v2/pet/923749237121")
+                .then()
                 .assertThat().statusCode(200);
     }
 
